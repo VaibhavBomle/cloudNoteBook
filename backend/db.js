12fba1dbf150ad637bea3ab24ac4a9c329a4ccd5
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2022-12-11 12:35:22 +0530

    Topic - 43 Db and Express Server Setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require(`mongoose`);
+const mongoURL = "mongodb://localhost:27017/?readPreference=primary&appName=MongoDB&directConnection=true&tls=false";
+
+const connectToMongo =() =>{
+    mongoose.connect(mongoURL,()=>{
+        console.log("Connected to Mongo Successfully")
+    })
+}
+
+module.exports = connectToMongo
