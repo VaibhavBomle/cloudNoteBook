0ed3ee09ab8ffa7e1a64b9f9f4f29f2192e23bf5
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2022-12-21 08:40:43 +0530

    Topic - 44 , 45 - Creating router and schema, storing data into DB

---
+++ backend/routes/auth.js
@@ -0,0 +1,13 @@
+const User = require(`../models/User`);
+const express = require(`express`);
+const router = express.Router();
+
+// Create a User using: POST "/api/auth".   Not required auth
+router.post(`/`,(req,res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+     res.send(req.body);
+})
+
+module.exports = router
