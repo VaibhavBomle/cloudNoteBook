a1ca6de7eb4cc2b5793dcba1f3f1b1b0c18a285e
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2022-12-29 09:42:02 +0530

    Topic - 49 Hashing password using bcryptjs in Nodejs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,27 +2,58 @@ const User = require(`../models/User`);
 const express = require(`express`);
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "WearelearingREACTJS"
+
 
 // Create a User using: POST "/api/auth".   Not required auth
 router.post(`/`,[
     body('name',"Enter a valid Name").isLength({min:3}),
     body('password',"Enter a valid password").isLength({min: 5}),
     body('email',"Enter a valid email").isEmail()
-],(req,res)=>{
+], async (req,res)=>{
     console.log(req.body)
+    // If error return error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    
+   
+    try{
+
+      // Ge user from db
+    let user = await User.findOne({
+      email: req.body.email
+    })
+
+    if(user){
+      return res.status(400).json({error: "User with this email already exist !!"})
+    }
+
+    const salt =  await bcrypt.genSalt(10);
+    
+    const secretPassword = await bcrypt.hash(req.body.password,salt);
+
+    // create a user
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-      }).then(user => res.json(user))
-      .catch(error=> {
-        console.log("error====>"+error);
-        res.json({error: 'Please enter a unique value ',message : error.message})
-    });
+        password: secretPassword
+      })
+      
+    //   .then(user => res.json(user))
+    //   .catch(error=> {
+    //     console.log("error====>"+error);
+    //     res.json({error: 'Please enter a unique value ',message : error.message})
+    // });
+
+    res.json(user)
+    }catch(error){
+       console.log("error ===>"+error.message);
+       res.status(500).send("Error occcur")
+    }
 })
 
 module.exports = router
